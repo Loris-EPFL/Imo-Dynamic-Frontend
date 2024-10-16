91feaeb8caa1a28b1ed937217e4bea9b3f930f16
ltran <loris.tran@epfl.ch>
2024-10-16 23:34:54 +0200

    feat: added total supply and amount burned

--- src/pages/Token/Token.js
+++ src/pages/Token/Token.js
@@ -12,21 +12,60 @@ import RealEstate from '../../components/RealEstate/RealEstate'
 import Team from '../../components/Team/Team'
 import Project from '../../components/Project/Project'
 import Roadmap from '../../components/Roadmap/Roadmap'
+import { useQuery, useQueryClient } from 'wagmi';
+import { chain } from 'wagmi';
+import { useAccount } from 'wagmi';
+import { useReadContract } from 'wagmi';
+import { IMO_TOKEN_ADDRESS_BASE } from '../../components/Stake/ContractAdress';
+import IMO_LZERC20 from '../../abi/IMO_LZERC20.json';
+import './Token.css';
+import { formatBigIntToDecimal } from "../../components/Stake/formatBigIntToDecimal";
 
 
 function Token() {
+  const { data: accountData } = useAccount();
+  const { data: totalSupplyData } = useReadContract({
+    address: IMO_TOKEN_ADDRESS_BASE,
+    abi: IMO_LZERC20,
+    functionName: 'totalSupply',
+  });
+
+
+
   return (
-    <main className='main' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+    <main className='main' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', marginTop: '100px' }}>
       <div className="token-presentation" style={{ textAlign: 'center' }}>
-        <h1 style={{ marginBottom: '20px' }}>Token Presentation</h1>
+        <div className="token-info">
+          <div className="deployment-details">
+          <h2>$IMO</h2>
+              <div className="chain-logos">
+              <img src="/favicon.ico" alt="IMO Token Logo" />
+              </div>
+            </div>
+          <div className="deployment-info">
+            <p>Deployed on:</p>
+            <div className="deployment-details">
+              <h2>Base</h2>
+              <div className="chain-logos">
+                <img src="/base_logo.png" alt="Base Logo" />
+              </div>
+            </div>
+          </div>
+          <div className="supply-info">
+            <h3>Supplies</h3>
+            <p>Initial Total Supply: 20,000,000 $IMO</p>
+            <p>Current Supply: {formatBigIntToDecimal(totalSupplyData).toString() ?? 'Loading...'}  $IMO</p>
+            <p>Total Burned: {formatBigIntToDecimal(window.BigInt(20000000*1e18) - totalSupplyData).toString() ?? 'Loading...'}  $IMO</p>
+          </div>
+        </div>
+
         <div className="chart-container" style={{ width: '80vw', height: '80vh' }}>
           <div id="dexscreener-embed" style={{ width: '100%', height: '100%' }}>
             <iframe src="https://dexscreener.com/base/0x007bb7a4bfc214df06474e39142288e99540f2b3?embed=1&theme=dark" style={{ width: '100%', height: '100%', border: 'none' }}></iframe>
           </div>
         </div>
         <div className="hidden-components">
-
-              <RealEstate />
+        <RealEstate />
                <DataScience />
                <Presentation />
                <Partners />
@@ -39,8 +78,8 @@ function Token() {
                <Team />
                <Roadmap />
                <Animation />
-          </div>
-      </div>
+        </div>
+     </div>
     </main>
   );
 }
